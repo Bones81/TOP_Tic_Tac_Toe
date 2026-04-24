e53d8a0f98425b705f39a00b865cf8ed551196f7
Nathan Freeman <nathanlfreeman@gmail.com>
2026-04-24 17:15:59 -0500

    Add DOMContentLoaded wrapper to ensure script runs after DOMContentLoaded. Add array spreader constructor to querySelectorAll Nodelist to access individual cells on click

--- script.js
+++ script.js
@@ -1,107 +1,116 @@
-// gameboard object
-  // gameboard array with 9 boxes
-const gameboard = (function() {
-  let currentBoard = ["", "", "", "", "", "", "", "", ""];
-  
-  const getBoard = () => currentBoard;
-  
-  const markBoard = (index, playerMark) => {
-    if (currentBoard[index] === "") {
-      currentBoard[index] = playerMark;
-    } else if (currentBoard[index] !== "") {
-      console.log("This box is already marked. Please choose another one.");
-    }
-  }
-
-  const resetBoard = () => {
-    currentBoard = ["", "", "", "", "", "", "", "", ""];
-  }
-
-  const checkBoard = () => {
-    // Implementation for checking win conditions
-    const checkTie = () => {
-      if (!currentBoard.includes("")) {
-        console.log("It's a draw!");
-        resetBoard();
+document.addEventListener('DOMContentLoaded', () => {
+    // gameboard object
+    // gameboard array with 9 boxes
+  const gameboard = (function() {
+    let currentBoard = ["", "", "", "", "", "", "", "", ""];
+    
+    const getBoard = () => currentBoard;
+    
+    const markBoard = (index, playerMark) => {
+      if (currentBoard[index] === "") {
+        currentBoard[index] = playerMark;
+      } else if (currentBoard[index] !== "") {
+        console.log("This box is already marked. Please choose another one.");
       }
     }
-    const checkWin = () => {
-      const winConditions = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
-        [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
-        [0, 4, 8], [2, 4, 6]             // diagonals
-      ];
 
-      for (let condition of winConditions) {
-        if (currentBoard[condition[0]] && currentBoard[condition[0]] === currentBoard[condition[1]] && currentBoard[condition[0]] === currentBoard[condition[2]]) {
-          const winningPlayer = () => {
-            if (currentBoard[condition[0]] === "X") {
-              return players()[0].name;
-            } else {
-              return players()[1].name;
-            }
-          }
-          console.log(`${winningPlayer()} wins!`);
+    const resetBoard = () => {
+      currentBoard = ["", "", "", "", "", "", "", "", ""];
+    }
+
+    const checkBoard = () => {
+      // Implementation for checking win conditions
+      const checkTie = () => {
+        if (!currentBoard.includes("")) {
+          console.log("It's a draw!");
           resetBoard();
-          return;
         }
       }
+      const checkWin = () => {
+        const winConditions = [
+          [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+          [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+          [0, 4, 8], [2, 4, 6]             // diagonals
+        ];
 
-      checkTie();
+        for (let condition of winConditions) {
+          if (currentBoard[condition[0]] && currentBoard[condition[0]] === currentBoard[condition[1]] && currentBoard[condition[0]] === currentBoard[condition[2]]) {
+            const winningPlayer = () => {
+              if (currentBoard[condition[0]] === "X") {
+                return players()[0].name;
+              } else {
+                return players()[1].name;
+              }
+            }
+            console.log(`${winningPlayer()} wins!`);
+            resetBoard();
+            return;
+          }
+        }
+
+        checkTie();
+      }
+      checkWin();
     }
-    checkWin();
-  }
 
-  return {getBoard, markBoard, checkBoard, resetBoard};
-})();
+    return {getBoard, markBoard, checkBoard, resetBoard};
+  })();
 
 
-// player objects
-const players = (playerOneName = "Player 1", playerTwoName = "Player 2") => [
-  { name: playerOneName, mark: "X" },
-  { name: playerTwoName, mark: "O" }
-];
+  // player objects
+  const players = (playerOneName = "Player 1", playerTwoName = "Player 2") => [
+    { name: playerOneName, mark: "X" },
+    { name: playerTwoName, mark: "O" }
+  ];
 
-const gameController = function() {
+  const gameController = function() {
 
-  const playTurn = (index) => {
-    const currentPlayer = players()[currentPlayerIndex];
-    gameboard.markBoard(index, currentPlayer.mark);
-    currentPlayerIndex === 0 ? currentPlayerIndex = 1 : currentPlayerIndex = 0;
-    console.log(`${currentPlayer.name} played at index ${index + 1}`);
+    const playTurn = (index) => {
+      const currentPlayer = players()[currentPlayerIndex];
+      gameboard.markBoard(index, currentPlayer.mark);
+      currentPlayerIndex === 0 ? currentPlayerIndex = 1 : currentPlayerIndex = 0;
+      console.log(`${currentPlayer.name} played at index ${index + 1}`);
+      logBoard();
+      gameboard.checkBoard();
+    }
+
+    const resetGame = () => {
+      gameboard.resetBoard();
+      currentPlayerIndex = 0;
+      console.log("Game reset!");
+      logBoard();
+    }
+
+    const logBoard = () => {
+      const row1 = `${gameboard.getBoard()[0]} | ${gameboard.getBoard()[1]} | ${gameboard.getBoard()[2]}`;
+      const row2 = `${gameboard.getBoard()[3]} | ${gameboard.getBoard()[4]} | ${gameboard.getBoard()[5]}`;
+      const row3 = `${gameboard.getBoard()[6]} | ${gameboard.getBoard()[7]} | ${gameboard.getBoard()[8]}`;
+      console.log(row1);
+      console.log(row2);
+      console.log(row3);
+    }
+
+    resetGame();
     logBoard();
-    gameboard.checkBoard();
+
+
+    return {resetGame, playTurn}
   }
 
-  const resetGame = () => {
-    gameboard.resetBoard();
-    currentPlayerIndex = 0;
-    console.log("Game reset!");
-    logBoard();
-  }
+  const game = gameController();
 
-  const logBoard = () => {
-    const row1 = `${gameboard.getBoard()[0]} | ${gameboard.getBoard()[1]} | ${gameboard.getBoard()[2]}`;
-    const row2 = `${gameboard.getBoard()[3]} | ${gameboard.getBoard()[4]} | ${gameboard.getBoard()[5]}`;
-    const row3 = `${gameboard.getBoard()[6]} | ${gameboard.getBoard()[7]} | ${gameboard.getBoard()[8]}`;
-    console.log(row1);
-    console.log(row2);
-    console.log(row3);
-  }
+  const displayController = function() {
+    const cells = [...document.querySelectorAll(".cell")];
+    cells.forEach((cell, index) => {
+      cell.addEventListener("click", () => {
+        console.log(`Cell ${index + 1} clicked`);
+        game.playTurn(index);
+      });
+    })
+    const resetButton = document.querySelector("#reset-button");
+    resetButton.addEventListener("click", () => {
+      game.resetGame();
+    });
+  }();  
 
-  resetGame();
-  logBoard();
-  playTurn(0);
-  playTurn(4);
-  playTurn(2);
-  playTurn(1);
-  playTurn(3);
-  playTurn(5);
-  playTurn(7);
-  playTurn(6);
-  playTurn(8);
-
-  return {resetGame, playTurn}
-}
-
-const game = gameController();
+});
